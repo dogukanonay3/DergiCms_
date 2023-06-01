3719c6cbc1e13f42d3e92e280998c09a7348e7bb
dogukanonay3 <donay3307@gmail.com>
2023-06-01 22:56:28 +0300

    UpdateAPI
    .

--- DergiAPI/Program.cs
+++ DergiAPI/Program.cs
@@ -1,15 +1,27 @@
+using DergiOrtak;
+using DergiOrtak.DataAccsess;
+using DergiOrtak.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DergiDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<IDataHandler, DataHandler>();
+builder.Services.AddScoped<IDergiService, DergiService>();
+builder.Services.AddScoped<ISayiService, SayiService>();
+builder.Services.AddScoped<IMakaleService, MakaleService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
